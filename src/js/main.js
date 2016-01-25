d2f20c5ec79179e37103d44ee1ce4ed866f8f9f7
Alex Mazzucca <alexmazzucca@Alexanders-iMac.local>
2016-01-25 12:04:01 -0500

    Cleaned and updated.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,24 @@
-$().ready(function() {
+(function ( $ ) {
+
+	$.Window = $(window);
+	var $mobileBreakpoint = 767,
+		$windowSize;
+
+	function sizeCheck(){
+		if($.Window.width() <= $mobileBreakpoint){
+			$windowSize = 'mobile';
+		}else{
+			$windowSize = 'desktop';
+		}
+	}
+
+	$.Window.bind("load resize", function() {
+		sizeCheck();
+	});
 
 	/*-------------------------------------
-	Comment
+	Plugins
 	-------------------------------------*/
+	
 
-});
+}(jQuery));
