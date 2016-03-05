ef7de3c7b350e5bed643472ab9e4a225af09fd36
Alex Mazzucca <alexmazzucca@Alexanders-iMac.local>
2016-03-04 20:04:54 -0500

    Refinement to mobile/desktop nav function. More generic.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
 (function ( $ ) {
 
 	$.Window = $(window);
+
 	var $mobileBreakpoint = 767,
 		$windowSize;
+
+	/*-------------------------------------
+	Window Size
+	-------------------------------------*/
 
 	function sizeCheck(){
 		if($.Window.width() <= $mobileBreakpoint){
@@ -12,13 +17,77 @@
 		}
 	}
 
-	$.Window.bind("load resize", function() {
+	$(document).ready(function() {
+		sizeCheck();
+	});
+
+	$.Window.bind("resize", function() {
 		sizeCheck();
 	});
 
 	/*-------------------------------------
-	Plugins
+	SVG Substitutions
 	-------------------------------------*/
-	
+
+	if (!Modernizr.svg) {
+		//$(".footer-logo img").attr("src", "img/logo-footer.png");
+	}
+
+	/*-------------------------------------
+	Navigation
+	-------------------------------------*/
+
+	$.fn.navControl = function(options){
+
+		var $burger = $('#burger');
+
+		// Initialize functions based on viewport size
+
+		function navInit(){
+			if($windowSize == 'mobile'){
+				if(!$('body').hasClass('mobile-menu')){
+					navMobileInit();
+				}
+				if($('body').hasClass('desktop-menu')){
+					navDesktopReset();
+				}
+			}else{
+				if(!$('body').hasClass('desktop-menu')){
+					navDesktopInit();
+				}
+				if($('body').hasClass('mobile-menu')){
+					navMobileReset();
+				}
+			}
+		}
+
+		$.Window.bind('load resize', navInit);
+
+		// Specific viewport functions
+
+		function navMobileInit() {
+			navDesktopReset();
+			$('body').addClass('mobile-menu');
+		}
+
+		function navDesktopInit(){
+			navMobileReset();
+			$('body').addClass('desktop-menu');
+		}
+
+		// Reset DOM Modifications
+
+		function navMobileReset(){
+			$('body').removeClass('mobile-menu');
+		}
+
+		function navDesktopReset(){
+			$('body').removeClass('desktop-menu');
+		}
+	}
+
+	$(document).ready(function() {
+		$('header.main').navControl();
+	});
 
 }(jQuery));
