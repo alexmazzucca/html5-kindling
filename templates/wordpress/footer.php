65301184117690017d5a513d8482658619d67e43
Alex Mazzucca <alexmazzucca@archerconsulting.net>
2019-12-23 16:02:46 -0500

    Update footer.php

--- templates/wordpress/footer.php
+++ templates/wordpress/footer.php
@@ -12,13 +12,13 @@
 
     </main>
 
+    <?php wp_footer(); ?>
+
     <script>
         window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
         ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
     </script>
     <script src="https://www.google-analytics.com/analytics.js" async></script>
-
-    <?php endif; ?>
 
     </body>
 </html>
