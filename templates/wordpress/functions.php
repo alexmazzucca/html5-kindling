1303b871092d78f0f77407e2893cbb5ef039ef8e
Alex Mazzucca <alexmazzucca@archerconsulting.net>
2020-02-20 21:26:54 -0500

    JS path corrected

--- templates/wordpress/functions.php
+++ templates/wordpress/functions.php
@@ -103,7 +103,7 @@
 
 	function theme_name_scripts() {
 		wp_enqueue_style( 'main-styles', get_stylesheet_uri(), '', '1' );
-		wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/js/main.min.js', array(), '3', true );
+		wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/js/main.js', array(), '3', true );
 	}
 
 	add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
