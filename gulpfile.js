99921921071cdd217ad5cd653112f1acd66af15b
Alex Mazzucca <alexmazzucca@archerconsulting.net>
2020-02-25 15:04:44 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -141,7 +141,7 @@ function changeProjectAddress(cb){
 	return gulp.src('./settings.json')
 		.pipe(jsonModify({
 				key: 'address',
-				value: settings.type
+				value: settings.address
 			}))
 		.pipe(gulp.dest('./'));
 
