2b7ee5bdc1b2882aa0f19c40f9f4269f2f7234c5
Alex Mazzucca <alexmazzucca@archerconsulting.net>
2020-04-30 14:04:34 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -123,24 +123,27 @@ function promptForWordpressDetails(cb){
 	}
 }
 
-function promptForInitialDeployentDetails(cb){
-	if(settings.server == 'yes'){
-		return gulp.src('./package.json')
-			.pipe(prompt.prompt([
-			{
-				type: 'list',
-				name: 'staging',
-				message: 'Is there a staging environment for this site?',
-				choices: ['yes', 'no']
-			}
-			], function(res){
-				settings.staging = res.staging;
-				cb();
-			}))
-			.pipe(gulp.dest('./'))
-	}else{
-		cb();
-	}
+function initialPromptForDeploymentOptions(){
+	return gulp.src('./package.json')
+		.pipe(prompt.prompt([
+		{
+			type: 'list',
+			name: 'server_live',
+			message: 'Would you like to configure a production (live) server?',
+			choices: ['yes', 'no']
+		},
+		{
+			type: 'list',
+			name: 'server_staging',
+			message: 'Would you like to configure a staging server?',
+			choices: ['yes', 'no']
+		}
+		], function(res){
+			settings.server_live = res.server_live;
+			settings.server_staging = res.server_staging;
+			cb();
+		}))
+		.pipe(gulp.dest('./'))
 }
 
 function promptForStagingDeployentDetails(cb){
@@ -407,7 +410,7 @@ const setupProject = gulp.series(
 	initialPromptForProjectInfo,
 	promptForSiteDetails,
 	promptForWordpressDetails,
-	promptForInitialDeployentDetails,
+	initialPromptForDeploymentOptions,
 	promptForStagingDeployentDetails,
 	promptForLiveDeployentDetails,
 	updateAdditionalProjectInfo,
