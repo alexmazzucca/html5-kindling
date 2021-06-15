cc0f7e4fb9bbc929d0ebc786a8014b0e9aa7115d
Alex Mazzucca <alexmazzucca@archerinteractive.com>
2021-06-15 12:54:14 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -496,7 +496,6 @@ const setupProject = gulp.series(
 	promptForWordpressDetails,
 	updateAdditionalProjectInfo,
 	renameWorkspaceFile,
-	removeWorkspaceFile,
 	updatePackageInfo,
 	updateProjectSettings,
 	updateGulpFile,
